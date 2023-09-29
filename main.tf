662f4d604d2c8d18f301eec3fcc7a2e8437c9f58
Andreas <andreas.landerer@vontobel.com>
2023-09-29 09:13:26 +0200

    Added subnets

--- main.tf
+++ main.tf
@@ -13,3 +13,17 @@ resource "azurerm_virtual_network" "azure-openai-vnet" {
   location = azurerm_resource_group.azure-openai-rg.location
   resource_group_name = azurerm_resource_group.azure-openai-rg.name
 }
+
+resource "azurerm_subnet" "azure-openai-compute-subnet" {
+  name = "compute-subnet"
+  resource_group_name = azurerm_resource_group.azure-openai-rg.name
+  virtual_network_name = azurerm_virtual_network.azure-openai-vnet.name
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "azure-openai-endpoint-subnet" {
+  name = "endpoint-subnet"
+  resource_group_name = azurerm_resource_group.azure-openai-rg.name
+  virtual_network_name = azurerm_virtual_network.azure-openai-vnet.name
+  address_prefixes = ["10.0.2.0/24"]
+}
