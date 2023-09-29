58d8ca948d4bc20b8bc9dfbf5a3c79851f92d4bf
Andreas <andreas.landerer@vontobel.com>
2023-09-29 08:51:03 +0200

    Added vnet

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "azure-openai-rg" {
   name = "azure-openai-rg"
   location = "Switzerland North"
 }
+
+resource "azurerm_virtual_network" "azure-openai-vnet" {
+  name = "azure-openai-vnet"
+  address_space = ["10.0.0.0/16"]
+  location = azurerm_resource_group.azure-openai-rg.location
+  resource_group_name = azurerm_resource_group.azure-openai-rg.name
+}
